d97283938731b1a6d6443e28e5f991ce4aee0d15
Ignacio Ferrer <nachopad31@hotmail.com>
2015-11-23 00:10:33 +0100

    FootballApp v.0.1

--- app/src/main/java/nach/com/myprojects/repasoexamen/ActivityJugadores.java
+++ app/src/main/java/nach/com/myprojects/repasoexamen/ActivityJugadores.java
@@ -1,11 +1,14 @@
 package nach.com.myprojects.repasoexamen;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -13,16 +16,38 @@ import java.util.ArrayList;
 
 public class ActivityJugadores extends Activity {
     GridView gV;
+    Button boton;
+    String equipo, liga, jugador;
     String[] nombres={"DeGea","Rojo","Jones","Smalling","Darmian","Schweinsteiger","Mata","Schneiderlin","Memphis",
             "Martial","Rooney","Lingard","Young","Felaini","Herrera","Bolt"};
     int[] images={R.drawable.degea,R.drawable.rojo,R.drawable.jones,R.drawable.smalling,R.drawable.darmian,R.drawable.schweins,
             R.drawable.mata,R.drawable.schneiderlin,R.drawable.memphis,R.drawable.martial,R.drawable.rooney,R.drawable.lingard,
             R.drawable.young,R.drawable.fellaini,R.drawable.herrera,R.drawable.bolt};
+
+    @Override
+    public void finish() {
+        Intent i = new Intent();
+        i.putExtra("jugadorIntent",jugador);
+        i.putExtra("equipoIntent",equipo);
+        i.putExtra("ligaIntent",liga);
+        setResult(RESULT_OK, i);
+        super.finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_jugadores);
 
+        Bundle b =getIntent().getExtras();
+        if(b!=null){
+            liga = b.getString("ligaIntent");
+            equipo = b.getString("equipoIntent");
+            Toast t=Toast.makeText(getApplicationContext(),"Liga: "+liga+" Equipo: "+equipo,Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        boton = (Button)findViewById(R.id.buttonEquipos);
         gV = (GridView)findViewById(R.id.gridView);
 
         Adaptador adaptador = new Adaptador(this,getJugadores());
@@ -31,10 +56,18 @@ public class ActivityJugadores extends Activity {
         gV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),nombres[position],Toast.LENGTH_SHORT);
+                Toast t=Toast.makeText(getApplicationContext(), nombres[position], Toast.LENGTH_SHORT);
+                t.show();
+                jugador=nombres[position];
             }
         });
 
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     private ArrayList<Jugador> getJugadores(){
@@ -66,4 +99,5 @@ public class ActivityJugadores extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/nach/com/myprojects/repasoexamen/ActivityLigasEquipos.java
+++ app/src/main/java/nach/com/myprojects/repasoexamen/ActivityLigasEquipos.java
@@ -1,6 +1,9 @@
 package nach.com.myprojects.repasoexamen;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +21,9 @@ import java.util.Arrays;
 public class ActivityLigasEquipos extends Activity {
 
     private static int REQUEST_CODE=1;
+    private static int UNITED_CODE=2;
+    private static int CITY_CODE=3;
+    private static int EVERTON_CODE=4;
 
     Spinner ligasSP,equiposSP;
     ArrayAdapter<String> adap_ligas,adap_equipos;
@@ -25,7 +31,6 @@ public class ActivityLigasEquipos extends Activity {
     String liga="";
     String equipo="";
     Button jugadoresBoton;
-    TextView ligaTexto,equipoTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,12 +99,10 @@ public class ActivityLigasEquipos extends Activity {
                         aListEquipos = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.ligue1)));
                         break;
                 }
+                liga = ligasSP.getSelectedItem().toString();
                 adap_equipos.clear();
                 adap_equipos.addAll(aListEquipos);
                 equiposSP.setAdapter(adap_equipos);
-
-                liga = ligasSP.getSelectedItem().toString();
-                equipo = equiposSP.getSelectedItem().toString();
             }
 
             @Override
@@ -108,15 +111,93 @@ public class ActivityLigasEquipos extends Activity {
             }
 
         });
+
+        equiposSP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                equipo = equiposSP.getSelectedItem().toString();
+                Toast t = Toast.makeText(getApplication(), "Liga: " + liga + " Equipo: " + equipo, Toast.LENGTH_SHORT);
+                t.show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void comprobarBoton(){
         jugadoresBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(ActivityLigasEquipos.this,ActivityJugadores.class);
+                Intent i = new Intent(ActivityLigasEquipos.this, ActivityJugadores.class);
+                i.putExtra("ligaIntent", liga);
+                i.putExtra("equipoIntent", equipo);
                 startActivityForResult(i, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==RESULT_OK&&requestCode==REQUEST_CODE){
+            String liga = data.getExtras().getString("ligaIntent");
+            String equipo = data.getExtras().getString("equipoIntent");
+            String jugador = data.getExtras().getString("jugadorIntent");
+            Toast t=Toast.makeText(getApplicationContext(),"Liga: "+liga+
+                    " Equipo: "+equipo+" Jugador: "+jugador
+                   , Toast.LENGTH_SHORT);
+            t.show();
+
+            notificacionEquipo();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void notificacionEquipo(){
+        Notification.Builder builder = new Notification.Builder(this);
+
+        builder.setContentTitle("Equipo preferido");
+        builder.setContentText("Elige tu equipo preferido entre estas opciones");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        Notification.InboxStyle iS = new Notification.InboxStyle();
+        iS.addLine("Elige bien");
+        iS.addLine("Como no cojas la correcta...");
+        iS.addLine("Déle PUTO");
+
+        builder.setStyle(iS);
+
+        String intent;
+
+        //Opción United.
+        Intent iUntd = new Intent(this,ActivityLigasEquipos.class);
+        iUntd.putExtra("UntdIntent", "Sabía que ibas a elegir la correcta :_D");
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,UNITED_CODE,iUntd,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent);
+        builder.addAction(R.mipmap.ic_launcher, "M.UNITED", pendingIntent);
+
+        //Opción City
+        Intent iCity = new Intent(this,ActivityLigasEquipos.class);
+        iCity.putExtra("CityIntent", "Te clavaría un naipe en la yugular!");
+        PendingIntent pendingIntent2 = PendingIntent.getActivity(this,CITY_CODE,iCity,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent2);
+        builder.addAction(R.mipmap.ic_launcher, "M.CITY", pendingIntent2);
+
+        //Opción City
+        Intent iEverton = new Intent(this,ActivityLigasEquipos.class);
+        iCity.putExtra("EvertonIntent", "MEH!");
+        PendingIntent pendingIntent3 = PendingIntent.getActivity(this,EVERTON_CODE,iEverton,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent3);
+        builder.addAction(R.mipmap.ic_launcher, "M.EVERTON", pendingIntent3);
+
+        NotificationManager nM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        nM.notify(2,builder.build());
+
+
     }
 }
